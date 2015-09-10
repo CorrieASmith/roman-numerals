0cc5c4d1fb9200e7bd81155de09ab3dace9fb7ea
Corrie Smith & Brian Lau <student@epicodus.com>
2015-09-10 11:20:56 -0700

    use output.join to join roman numeral elements together

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,42 @@
 var romanNumeral = function(number) {
   var output = [];
+
   while(number >= 1000) {
     output.push("M");
     number -= 1000;
   }
+
   while(number >= 500) {
     output.push("D");
     number -= 500;
   }
+
   while(number >= 100) {
     output.push("C");
     number -= 100;
   }
+
   while(number >= 50) {
     output.push("L");
     number -= 50;
   }
+
   while(number >= 10) {
     output.push("X");
     number -= 10;
   }
+
   while(number >= 5) {
     output.push("V");
     number -= 5;
   }
+
   while(number >= 1) {
     output.push("I");
     number -= 1;
   }
-  return output;
+  
+  return output.join("");
 };
 
 
