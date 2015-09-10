bb7117a26e80b9b732fb03d3bdf3f8fa5195da23
Corrie Smith & Brian Lau <student@epicodus.com>
2015-09-10 11:16:45 -0700

    reduced specs and js code to one single variable, front end quickfixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
-var romanNumeralI = function(number) {
+var romanNumeral = function(number) {
   var output = [];
+  while(number >= 1000) {
+    output.push("M");
+    number -= 1000;
+  }
+  while(number >= 500) {
+    output.push("D");
+    number -= 500;
+  }
+  while(number >= 100) {
+    output.push("C");
+    number -= 100;
+  }
+  while(number >= 50) {
+    output.push("L");
+    number -= 50;
+  }
+  while(number >= 10) {
+    output.push("X");
+    number -= 10;
+  }
+  while(number >= 5) {
+    output.push("V");
+    number -= 5;
+  }
   while(number >= 1) {
     output.push("I");
     number -= 1;
@@ -7,56 +31,16 @@ var romanNumeralI = function(number) {
   return output;
 };
 
-var romanNumeralV = function(number) {
-  var output = [];
-  while(number >= 5) {
-    output.push("V");
-    number -= 5;
-  }
-  return output;
-};
 
-var romanNumeralX = function(number) {
-  var output = [];
-  while(number >= 10) {
-    output.push("X");
-    number -= 10;
-  }
-  return output;
-};
+$(document).ready(function() {
+  $("form#romanNumerals").submit(function(event) {
+    var number = parseInt($("input#number").val());
+    var output = romanNumeral(number);
 
-var romanNumeralL = function(number) {
-  var output = [];
-  while(number >= 50) {
-    output.push("L");
-    number -= 50;
-  }
-  return output;
-};
+  $(".number").text(number);
+  $(".output").text(output);
 
-var romanNumeralC = function(number) {
-  var output = [];
-  while(number >= 100) {
-    output.push("C");
-    number -= 100;
-  }
-  return output;
-};
-
-var romanNumeralD = function(number) {
-  var output = [];
-  while(number >= 500) {
-    output.push("D");
-    number -= 500;
-  }
-  return output;
-};
-
-var romanNumeralM = function(number) {
-  var output = [];
-  while(number >= 1000) {
-    output.push("M");
-    number -= 1000;
-  }
-  return output;
-};
+    $("#output").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,41 +1,5 @@
-describe('romanNumeralI', function() {
-  it("takes roman numeral I and adds it to numbers 1-4", function() {
-    expect(romanNumeralI(1)).to.eql(["I"])
-  });
-});
-
-describe('romanNumeralV', function() {
-  it("takes roman numeral V and adds it to numbers 5-9", function() {
-    expect(romanNumeralV(5)).to.eql(["V"])
-  });
-});
-
-describe('romanNumeralX', function() {
-  it("takes roman numeral X and adds it to numbers 10-19", function() {
-    expect(romanNumeralX(10)).to.eql(["X"])
-  });
-});
-
-describe('romanNumeralL', function() {
-  it("takes roman numeral L and adds it to numbers 50-99", function() {
-    expect(romanNumeralL(50)).to.eql(["L"])
-  });
-});
-
-describe('romanNumeralC', function() {
-  it("takes roman numeral C and adds it to numbers 100-499", function() {
-    expect(romanNumeralC(100)).to.eql(["C"])
-  });
-});
-
-describe('romanNumeralD', function() {
-  it("takes roman numeral D and adds it to numbers 500-999", function() {
-    expect(romanNumeralD(500)).to.eql(["D"])
-  });
-});
-
-describe('romanNumeralM', function() {
-  it("takes roman numeral M and adds it to numbers 1000-3999", function() {
-    expect(romanNumeralM(1000)).to.eql(["M"])
+describe('romanNumeral', function() {
+  it("takes a number between 1 and 3999 and converts it into roman numerals", function() {
+    expect(romanNumeral(1)).to.eql(["I"])
   });
 });
