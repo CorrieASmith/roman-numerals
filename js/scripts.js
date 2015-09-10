d64d31b8fcb7b0a3e350db5b134ea1cc71345a81
Corrie Smith & Brian Lau <student@epicodus.com>
2015-09-10 10:04:09 -0700

    use parseInt to post result to front end page

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ var countUp = function(countTo, countBy) {
   for(var i = countBy; i <= countTo; i += countBy) {
     output.push(i)
   }
-  return output
+  return output;
 };
 
 $(document).ready(function() {
   $("form#count-up-by").submit(function(event) {
-    var countBy = $("input#countBy").val();
-    var countTo = $("input#countTo").val();
-    var output = countUp(countBy, countTo);
+    var countBy = parseInt($("input#countBy").val());
+    var countTo = parseInt($("input#countTo").val());
+    var output = countUp(countTo, countBy);
 
   $(".countBy").text(countBy);
   $(".countTo").text(countTo);
